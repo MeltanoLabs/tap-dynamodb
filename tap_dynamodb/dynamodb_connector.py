bfb2551aa53f3bd06fff4b3455b3d421e1bf3e54
pnadolny13 <patnadolny@gmail.com>
2023-06-06 16:23:41 -0400

    support overriding scan kwargs for specific tables

--- tap_dynamodb/dynamodb_connector.py
+++ tap_dynamodb/dynamodb_connector.py
@@ -61,8 +61,12 @@ class DynamoDbConnector(AWSBotoConnector):
             return tables
 
     def get_items_iter(
-        self, table_name: str, scan_kwargs: dict = {"ConsistentRead": True}
+        self, table_name: str, scan_kwargs_override: dict
     ):
+        scan_kwargs = scan_kwargs_override.copy()
+        if "ConsistentRead" not in scan_kwargs:
+            scan_kwargs["ConsistentRead"] = True
+
         table = self.resource.Table(table_name)
         try:
             done = False
@@ -85,10 +89,16 @@ class DynamoDbConnector(AWSBotoConnector):
             )
             raise
 
-    def _get_sample_records(self, table_name: str, sample_size: int) -> list:
+    def _get_sample_records(self, table_name: str, sample_size: int, scan_kwargs_override: dict) -> list:
+        scan_kwargs = scan_kwargs_override.copy()
         sample_records = []
+        if "ConsistentRead" not in scan_kwargs:
+            scan_kwargs["ConsistentRead"] = True
+        if "Limit" not in scan_kwargs:
+            scan_kwargs["Limit"] = sample_size
+
         for batch in self.get_items_iter(
-            table_name, scan_kwargs={"Limit": sample_size, "ConsistentRead": True}
+            table_name, scan_kwargs
         ):
             sample_records.extend(batch)
             if len(sample_records) >= sample_size:
@@ -96,9 +106,9 @@ class DynamoDbConnector(AWSBotoConnector):
         return sample_records
 
     def get_table_json_schema(
-        self, table_name: str, sample_size, strategy: str = "infer"
+        self, table_name: str, sample_size, scan_kwargs: dict, strategy: str = "infer"
     ) -> dict:
-        sample_records = self._get_sample_records(table_name, sample_size)
+        sample_records = self._get_sample_records(table_name, sample_size, scan_kwargs)
 
         if not sample_records:
             raise EmptyTableException()
